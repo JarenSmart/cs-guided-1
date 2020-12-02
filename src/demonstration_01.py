b760ff6bfc5e76784570a7121b345e7acb340cfa
jaren <jarensmart@yahoo.com>
2020-12-01 19:21:38 -0500

    demo1 done

--- src/demonstration_01.py
+++ src/demonstration_01.py
@@ -8,6 +8,8 @@ Examples:
 - addition(-3, -6) ➞ -9
 - addition(7, 3) ➞ 10
 """
+
+
 def addition(a, b):
     # Your code here
-
+    a + b
--- src/demonstration_02.py
+++ src/demonstration_02.py
@@ -1,13 +1,14 @@
 """
-Challenge #2:
+   Challenge #2:
 
-Write a function that takes an integer `minutes` and converts it to seconds.
+   Write a function that takes an integer `minutes` and converts it to seconds.
 
-Examples:
-- convert(5) ➞ 300
-- convert(3) ➞ 180
-- convert(2) ➞ 120
-"""
+   Examples:
+   - convert(5) ➞ 300
+   - convert(3) ➞ 180
+   - convert(2) ➞ 120
+   """
+
+
 def convert(minutes):
     # Your code here
-
